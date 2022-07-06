4401ec35af3c5923a7271d833ef41222894b9171
Petr Verner <petr.verner1864@gmail.com>
2022-07-06 17:48:27 +0200

    add the first view
    Signed-off-by: Petr Verner <petr.verner1864@gmail.com>

--- english/web/views.py
+++ english/web/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello there, You're at the web index.")
