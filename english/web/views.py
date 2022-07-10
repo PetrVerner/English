fbca7f0105e54d416eccc4f1c6236d1ac1ed8e19
Petr Verner <petr.verner1864@gmail.com>
2022-07-10 19:27:59 +0200

    modify the views.py, urls.py and create the templates

--- english/web/views.py
+++ english/web/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
-
+from django.http import Http404
+from django.shortcuts import render
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello there, You're at the web index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'web/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist. :(")
+    return render(request, 'web/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "2 %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("3 %s." % question_id)
