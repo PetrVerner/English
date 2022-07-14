bbda57b49301b3240f5d8c3144bffdb5f413a2ad
Petr Verner <petr.verner1864@gmail.com>
2022-07-14 12:32:21 +0200

    add the first, easy test

--- english/web/views.py
+++ english/web/views.py
@@ -1,20 +1,23 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'web/index.html', context)
+class IndexView(generic.ListView):
+    template_name = "web/index.html"
+    context_object_name = "latest_question_list"
 
-def detail(request, question_id):
-    question = get_object_or_404 (Question, pk=question_id)
-    return render(request, 'web/detail.html', {'question': question})
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "web/results.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "web/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "web/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
